459d082c4c43dcc3118aba8886abdab8f73cd258
gfeiden <gregory.a.feiden@gmail.com>
2019-07-22 13:20:51 -0400

    added week 1, exercise 1

---
+++ Week 01/Exercise_01.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Exercise: Chapter 01, Kinder & Nelson
+
+    Download and run this script or, enter each line into an IPython console. 
+    There are a few errors in the script. Try to find and correct them. Make 
+    note of each error and explain (1) why it's an error, and (2) how to fix 
+    it. 
+    
+    NOTE: there may be more than one way to fix an error
+    
+    Once you have the script working, explain why the value for c has a decimal
+    and the values for a and b do not.
+
+
+Created on Mon Jul 22 11:02:19 2019
+
+@author: gafeiden
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+# compute the hypotenuse of a right triangle
+a = 3
+b = 4
+c = sqrt(a**2 + b**2)
+
+# print out the result
+print "The triangle's hypotenuse is:", c
+
