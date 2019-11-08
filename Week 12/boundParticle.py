2a6e269f9d4c995c31c9154e1b94a975d20daa25
Gregory Feiden <gregory.a.feiden@gmail.com>
2019-11-08 10:51:32 -0500

    final version with E iterator

--- Week 12/boundParticle.py
+++ Week 12/boundParticle.py
@@ -12,54 +12,61 @@ import matplotlib.pyplot as plt
 def potential(x, a):
     """ """
     if abs(x) <= a:
+        # position inside potential
         V0 = -83.0
     else:
+        # position outside potential
         V0 = 0.0
     return V0
 
-def wave_function_outside(x, kappa):
-    """ """ 
-    psi = np.exp(-kappa*abs(x))
-    dpsi_dx = -np.sign(x) * kappa * np.exp(-kappa*abs(x))
-    return psi, dpsi_dx
-
 def solve_wave_function(dx, x_left, x_right, a, E):
     """ """
+    # set constants in Schrodinger Eq.
     q = 0.0483
     kappa_2 = -q*E
     kappa   = np.sqrt(kappa_2)
     
+    # set initial conditions for x >> a using analytical solution
     xs   = [x_left]
     psi  = [np.exp(-kappa*abs(xs[-1]))]
     dpsi = [-np.sign(xs[-1]) * kappa * np.exp(-kappa*abs(xs[-1]))]
-    for n in range(int((x_right - x_left)/dx)):
-        x = xs[-1]
-        if abs(x) > a:
-            dpsi_new = dpsi[-1] + kappa_2*psi[-1]*dx
-            psi_new  = psi[-1]  + dpsi[-1]*dx
-            #psi_new, dpsi_new = wave_function_outside(x+dx, kappa)
-        else:
-            dpsi_new = dpsi[-1] + (q*potential(x, a) + kappa_2)*psi[-1]*dx
-            psi_new  = psi[-1]  + dpsi[-1]*dx
-        
-        x += dx
-        
-        xs.append(x)
-        psi.append(psi_new)
-        dpsi.append(dpsi_new)
     
-    xs   = np.array(xs)
-    psi  = np.array(psi)
-    dpsi = np.array(dpsi)
+    # set in position using Eulerian scheme
+    for n in range(int((x_right - x_left)/dx)):
+        # x position from previous step
+        x = xs[-1]
+        
+        # calculate new spatial derivative for wave function for step dx
+        dpsi_new = dpsi[-1] + (q*potential(x, a) + kappa_2)*psi[-1]*dx
+        
+        # calculate new wave function value for step dx
+        psi_new  = psi[-1]  + dpsi[-1]*dx
+        
+        # step in position from x_old to x_new
+        x += dx  
+        
+        xs.append(x)          # append updated x position
+        psi.append(psi_new)   # append new value for wavefunction 
+        dpsi.append(dpsi_new) # append new spatial derivative of wave function
+    
+    xs   = np.array(xs)   # convert position list to array
+    psi  = np.array(psi)  # convert wave function list to array
+    dpsi = np.array(dpsi) # convert wave function derivative list to array
+    
     return xs, psi, dpsi
 
-E = -20.0    
-a =  2.0
-x_match = a+0.2
-dx = 1.0e-3
 
+# set parameters of run
+a =  2.0       # potential size -a <= x <= +a
+x_match = 1.9  # position to match the left/right integrator
+dx = 1.0e-3    # absolute value of the position step size
+
+
+# Guess initial values for energy iterator
 E  = [-10.0, -20.0]
 delta = E[1] - E[0]
+
+# Energy iterator to solve for best fit E
 for n in range(20):
     dE = []
     
@@ -79,13 +86,14 @@ for n in range(20):
     DdE_DE = (dE[1] - dE[0])/(E[1] - E[0])
     E_test = E[0] - dE[0]/DdE_DE
     
+    # calculate wave function and positional derivatives for test Energy
     xs_left,  psi_left,  dpsi_left  = solve_wave_function( dx, -100., x_match, a, E_test)
     xs_right, psi_right, dpsi_right = solve_wave_function(-dx,  100., x_match, a, E_test)
     
+    # compute logarithmic derivative to check for convergence
     dE_test = (dpsi_left[-1]/psi_left[-1] - dpsi_right[-1]/psi_right[-1]) / (dpsi_left[-1]/psi_left[-1] + dpsi_right[-1]/psi_right[-1])
     
-    print(dE_test)
-    # check whether wavefunction is continuous at xmatch
+    # check whether wavefunction is continuous at x_match
     if (abs(dE_test) < 1.0e-5):
         # solution found, exit loop
         E_final = E_test
@@ -97,10 +105,13 @@ for n in range(20):
         E = [E_test, E_test + delta]
         print("Iteration {:02.0f}: No Match. Propose: E = {:+12.7f} MeV, deltaE = {:+12.7f} MeV".format(n, E_test, delta))
 
-#E_final = -15.07
+
+
+# create final curve using E_final (to be safe)
 xs_left,  psi_left,  dpsi_left  = solve_wave_function( dx, -100., x_match, a, E_final)
 xs_right, psi_right, dpsi_right = solve_wave_function(-dx,  100., x_match, a, E_final)
 
+# plot the wave function for the best fit particle Energy
 plt.plot(xs_left,  psi_left,  'o', ms=1.0)
 plt.plot(xs_right, psi_right, 'o', ms=1.0)
 plt.xlim(-a - 1.0, a + 1.0)
