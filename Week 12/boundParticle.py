1431eb1a11ec2fcc286350602305292cd02dfb1d
gfeiden <gregory.a.feiden@gmail.com>
2019-11-08 09:56:17 -0500

    energy iterator, rnd 2

--- Week 12/boundParticle.py
+++ Week 12/boundParticle.py
@@ -20,21 +20,22 @@ def potential(x, a):
 def wave_function_outside(x, kappa):
     """ """ 
     psi = np.exp(-kappa*abs(x))
-    dpsi_dx = -1.0*x/abs(x) * kappa*np.exp(-kappa*abs(x))
+    dpsi_dx = -np.sign(x) * kappa * np.exp(-kappa*abs(x))
     return psi, dpsi_dx
 
 def solve_wave_function(dx, x_left, x_right, a, E):
     """ """
     q = 0.0483
     kappa_2 = -q*E
+    kappa   = np.sqrt(kappa_2)
     
     xs   = [x_left]
-    psi  = [np.exp(-np.sqrt(kappa_2)*abs(xs[-1]))]
-    dpsi = [-1.0*(xs[-1])/abs(xs[-1]+1.0e-16) * np.sqrt(kappa_2) * np.exp(-np.sqrt(kappa_2)*abs(xs[-1]))]
+    psi  = [np.exp(-kappa*abs(xs[-1]))]
+    dpsi = [-np.sign(xs[-1]) * kappa * np.exp(-kappa*abs(xs[-1]))]
     for n in range(int((x_right - x_left)/dx)):
         x = xs[-1]
         if abs(x) > a:
-            psi_new, dpsi_new = wave_function_outside(x+dx, np.sqrt(kappa_2))
+            psi_new, dpsi_new = wave_function_outside(x+dx, kappa)
         else:
             dpsi_new = dpsi[-1] + (q*potential(x, a) + kappa_2)*psi[-1]*dx
             psi_new  = psi[-1] + dpsi[-1]*dx
@@ -51,23 +52,13 @@ def solve_wave_function(dx, x_left, x_right, a, E):
     return xs, psi, dpsi
 
 E = -20.0    
-a =  2.0
+a =  1.5
 x_match = a
 dx = 1.0e-2
 
-xs = np.arange(-100., 100., 1.0e-2)
-V  = []
-for x in xs:
-    if abs(x) > a:
-        V.append(0)
-    else:
-        V.append(-83.0)
-V = np.array(V)
-
 E  = [-10.0, -20.0]
-dE = []
 delta = E[1] - E[0]
-for n in range(30):
+for n in range(20):
     dE = []
     
     # try a lower value for |E|
@@ -82,21 +73,35 @@ for n in range(30):
 
     dE.append((dpsi_left[-1]/psi_left[-1] - dpsi_right[-1]/psi_right[-1]) / (dpsi_left[-1]/psi_left[-1] + dpsi_right[-1]/psi_right[-1]))
 
-    # choose new E for a test run
-    #E_test = 
-    break
+    # bisect to find a new E for test run
+    DdE_DE = (dE[1] - dE[0])/(E[1] - E[0])
+    E_test = E[0] - dE[0]/DdE_DE
+    
+    xs_left,  psi_left,  dpsi_left  = solve_wave_function( dx, -100., x_match, a, E_test)
+    xs_right, psi_right, dpsi_right = solve_wave_function(-dx,  100., x_match, a, E_test)
+    
+    dE_test = (dpsi_left[-1]/psi_left[-1] - dpsi_right[-1]/psi_right[-1]) / (dpsi_left[-1]/psi_left[-1] + dpsi_right[-1]/psi_right[-1])
+    
+    print(dE_test)
     # check whether wavefunction is continuous at xmatch
-    if (abs(psi_left[-1] - psi_right[-1]) < 1.0e-6):
+    if (abs(dE_test) < 1.0e-8):
         # solution found, exit loop
+        E_final = E_test
+        print("Iteration {:02.0f}: Matched! E = {:+12.7f} MeV".format(n, E_final))
         break
     else:
         # select a new E and retry
-        pass
+        delta = -np.sign(dE_test)
+        E = [E_test, E_test + delta]
+        print("Iteration {:02.0f}: No Match. Propose: E = {:+12.7f} MeV, deltaE = {:+12.7f} MeV".format(n, E_test, delta))
 
-#plt.plot(xs, V, '-', lw=3, c="black")
+
+xs_left,  psi_left,  dpsi_left  = solve_wave_function( dx, -100., x_match, a, E_final)
+xs_right, psi_right, dpsi_right = solve_wave_function(-dx,  100., x_match, a, E_final)
+
 plt.plot(xs_left,  psi_left,  'o', ms=1.0)
 plt.plot(xs_right, psi_right, 'o', ms=1.0)
-plt.xlim(-a - 1.0, a + 1.0)
+#plt.xlim(-a - 1.0, a + 1.0)
 plt.xlabel("$x$ [fm]")
 plt.ylabel("$\psi(x)$")
 plt.show()
