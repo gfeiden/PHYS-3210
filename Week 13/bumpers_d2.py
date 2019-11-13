07c00d458f7eb7f46d1f83b1f0058047e0cd98c5
Gregory Feiden <gregory.a.feiden@gmail.com>
2019-11-13 10:54:33 -0500

    Many bounces, much fun

--- Week 13/bumpers_d2.py
+++ Week 13/bumpers_d2.py
@@ -16,16 +16,17 @@ def bumperForce(x):
           x -- list where x[0] == X, x[1] == Y
     
     """
-    Fx = 2.0*x[0]*x[1]**2*(1.0 - x[0]**2)*np.exp(-(x[0]**2 + x[1]**2))
-    Fy = 2.0*x[1]*x[0]**2*(1.0 - x[1]**2)*np.exp(-(x[0]**2 + x[1]**2))
+    Fx = -2.0*x[0]*x[1]**2*(1.0 - x[0]**2)*np.exp(-(x[0]**2 + x[1]**2))
+    Fy = -2.0*x[1]*x[0]**2*(1.0 - x[1]**2)*np.exp(-(x[0]**2 + x[1]**2))
     
     return [Fx, Fy]
 
 def solveBumpers(x0, v0, m=1.0, dt=1.0e-3):
     """ """
+    t = [0.0]
     x = [x0]
     v = [v0]
-    for n in range(int(100./dt)):
+    for n in range(int(1000./dt)):
         F  = bumperForce(x[-1])
         
         v_X = v[-1][0] + F[0]*dt/m
@@ -36,14 +37,16 @@ def solveBumpers(x0, v0, m=1.0, dt=1.0e-3):
         
         v.append([v_X, v_Y])
         x.append([x_X, x_Y])
+        t.append(t[-1] + dt)
     
+    t = np.array(t)
     x = np.array(x)
     v = np.array(v)
     
-    return x, v
+    return t, x, v
 
-x1, v1 = solveBumpers(x0=[0.9, 0.4], v0=[0.0, 0.0], m=0.5)
-x2, v2 = solveBumpers(x0=[-0.9, 0.4], v0=[0.0, 0.0], m=0.5)
+t, x1, v1 = solveBumpers(x0=[ 0.949, 0.0], v0=[-0.01, 0.01], m=0.1)
+t, x2, v2 = solveBumpers(x0=[-0.9, 0.4], v0=[0.1, 0.1], m=0.1)
 
 # plot potential contours
 x_v = np.arange(-10.0, 10.01, 0.01)
@@ -52,11 +55,11 @@ X_v, Y_v = np.meshgrid(x_v, y_v)
 Z_v = X_v**2 * Y_v**2 * np.exp(-(X_v**2 + Y_v**2))
 
 # PLOT TRAJECTORIES W/ POTENTIAL CONTOURS
-fig, ax = plt.subplots(1, 1, figsize=(10, 8))
+fig, ax = plt.subplots(1, 1)#, figsize=(10, 8))
 
-cb = ax.contourf(X_v, Y_v, Z_v, 10, cmap="Oranges")
-ax.plot(x1[:,0], x1[:,1], lw=2)
-ax.plot(x2[:,0], x2[:,1], lw=2)
+cb = ax.contourf(X_v, Y_v, Z_v, 10, cmap="Blues")
+ax.plot(x1[:,0], x1[:,1], lw=1)
+ax.plot(x2[:,0], x2[:,1], lw=1)
 
 ax.set_xlim(-3.0, 3.0)
 ax.set_xlabel("X Position")
@@ -65,6 +68,40 @@ ax.set_ylabel("Y Position")
 
 cbar = fig.colorbar(cb)
 cbar.ax.set_ylabel("Scattering Potential")
+
+fig.tight_layout()
+plt.show()
+
+# PLOT Position vs Time
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(t, x1[:,0], lw=1)
+ax[0].plot(t, x2[:,0], lw=1)
+ax[0].set_xlabel("Time [s]")
+ax[0].set_ylim(-3.0, 3.0)
+ax[0].set_ylabel("X Position")
+
+ax[1].plot(t, x1[:,1], lw=1)
+ax[1].plot(t, x2[:,1], lw=1)
+ax[1].set_xlabel("Time [s]")
+ax[1].set_ylim(-3.0, 3.0)
+ax[1].set_ylabel("Y Position")
+
+fig.tight_layout()
+plt.show()
+
+# PLOT Velocity vs Position
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(x1[:,0], v1[:,0], lw=1)
+ax[0].plot(x2[:,0], v2[:,0], lw=1)
+ax[0].set_xlabel("X Position")
+ax[0].set_xlim(-3.0, 3.0)
+ax[0].set_ylabel("X Velocity")
+
+ax[1].plot(x1[:,1], v1[:,1], lw=1)
+ax[1].plot(x2[:,1], v2[:,1], lw=1)
+ax[1].set_xlabel("Y Position")
+ax[1].set_xlim(-3.0, 3.0)
+ax[1].set_ylabel("Y Velocity")
 
 fig.tight_layout()
 plt.show()
